adec786653ad554b3389cd406925a1c87c2b0c38
gyurel <gyurel@yahoo.com>
2023-01-20 00:34:46 +0100

    problem 9 Arrays

---
+++ Arrays Exercise/sortAnArrayByTwoCriteria.js
@@ -0,0 +1,25 @@
+function solve(arr){
+    arr.sort((a, b) => a.length - b.length || a.localeCompare(b));
+
+    console.log(arr.join('\n'));
+    // console.log('.................................');
+}
+
+solve(['alpha', 
+'beta', 
+'gamma']
+);
+
+solve(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']
+
+);
+
+solve(['test', 
+'Deny', 
+'omen', 
+'Default']
+);
