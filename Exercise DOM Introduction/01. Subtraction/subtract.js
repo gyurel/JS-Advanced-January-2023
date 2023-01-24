fe49158a95f5e5317e5cc617864e173f53106a62
gyurel <gyurel@yahoo.com>
2023-01-24 16:24:34 +0100

    problem 1 DOM

--- Exercise DOM Introduction/01. Subtraction/subtract.js
+++ Exercise DOM Introduction/01. Subtraction/subtract.js
@@ -1,3 +1,9 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNumber = document.getElementById('firstNumber');
+    let secondNumber = document.getElementById('secondNumber');
+    let resultElement = document.getElementById('result');
+
+    let result = Number(firstNumber.value) - Number(secondNumber.value);
+
+    resultElement.textContent = result;
 }
