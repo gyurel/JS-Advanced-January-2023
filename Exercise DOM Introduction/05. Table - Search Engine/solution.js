b57a0f212740ce3a597a6561ca932a4f791977d9
gyurel <gyurel@yahoo.com>
2023-01-26 16:05:15 +0100

    problem 5 DOM

--- Exercise DOM Introduction/05. Table - Search Engine/solution.js
+++ Exercise DOM Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,21 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
-   }
+      let tableRows = document.querySelectorAll('tbody tr');
+      let searchText = document.querySelector('#searchField').value;
+      
+      for (const row of tableRows) {
+         // debugger
+         for (const cell of Array.from(row.children)) {
+            if(cell.textContent.includes(searchText)){
+               cell.parentElement.classList.add('select');
+               break;
+            }else{
+               cell.parentElement.classList.remove('select');
+            };
+         };
+      };
+      // debugger
+      searchText = '';
+   };
 }
