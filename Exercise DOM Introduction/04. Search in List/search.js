a842d1130ed3bb51168b57e221f45d9aa93348b5
gyurel <gyurel@yahoo.com>
2023-01-26 13:55:18 +0100

    problem 4 DOM

--- Exercise DOM Introduction/04. Search in List/search.js
+++ Exercise DOM Introduction/04. Search in List/search.js
@@ -1,3 +1,25 @@
 function search() {
-   // TODO
+
+
+   let searchText = document.getElementById('searchText');
+   let towns = Array.from(document.getElementsByTagName('li'));
+   let resultText = document.getElementById('result');
+
+   let counter = 0;
+
+   for (const town of towns) {
+      // debugger
+      if(town.textContent.includes(searchText.value)){
+
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+         counter += 1;
+      }else{
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+
+   resultText.textContent = `${counter} matches found`;
+   
 }
