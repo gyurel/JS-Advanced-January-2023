96a99a30a8771712af2a17175b9216c6773270bb
gyurel <gyurel@yahoo.com>
2023-01-28 21:10:56 +0100

    problem 8 DOM

--- Exercise DOM Introduction/08. Generate Report/report.js
+++ Exercise DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,21 @@
 function generateReport() {
-    //TODO
+    let outputElement = document.getElementById('output');
+    let headRow = Array.from(document.querySelectorAll('thead tr th input'));
+    let tableRows = Array.from(document.querySelectorAll('tbody tr'));
+
+    let resultArr = [];
+
+    for (let row = 0; row < tableRows.length; row++) {
+        let currentTableRow = Array.from(tableRows[row].querySelectorAll('td'));
+        let currentTableObj = {};
+
+        for (let col = 0; col < headRow.length; col++) {
+            if(headRow[col].checked){
+                currentTableObj[headRow[col].name] = currentTableRow[col].textContent;
+                
+            }
+        }
+        resultArr.push(currentTableObj);
+    }
+    outputElement.textContent = JSON.stringify(resultArr);
 }
