025f8dc2164a4e8e4b6606bf66d8d81fa8f1004c
gyurel <gyurel@yahoo.com>
2023-01-29 14:33:41 +0100

    problem 1 Manipulations Events

--- Exercise DOM Manipulation and Events/01. Sections/script.js
+++ Exercise DOM Manipulation and Events/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+   let content = document.getElementById('content');
+
+   for (const element of words) {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.style.display = 'none';
+      p.textContent = element;
+
+      div.appendChild(p);
+
+      div.addEventListener('click', onClick);
+      content.appendChild(div);
+   }
+
+   function onClick(event){
+      let currentElement = event.target.querySelector('p');
+      currentElement.style.display = 'block';
+   }
 }
