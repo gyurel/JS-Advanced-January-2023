b6ec4769ddf64151ea80a069943f39de236902e6
gyurel <gyurel@yahoo.com>
2023-01-30 23:52:52 +0100

    problem 5 Manipulations and Events

--- Exercise DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
+++ Exercise DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,38 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    document.querySelectorAll('button')[0].addEventListener('click', encodeText);
+    document.querySelectorAll('button')[1].addEventListener('click', decodeText);
+
+    function encodeText(event){
+        let currentText = document.querySelectorAll('textarea')[0].value;
+
+        let encodedText = ''
+
+        for (let index = 0; index < currentText.length; index++) {
+            const currentChar = currentText[index];
+            const charCode = currentChar.charCodeAt(0);
+            const newChar = String.fromCharCode(charCode + 1);
+
+            encodedText += newChar; 
+        }
+
+        document.querySelectorAll('textarea')[1].value = encodedText;
+        document.querySelectorAll('textarea')[0].value = '';
+
+    }
+
+    function decodeText(event){
+        let currentText = document.querySelectorAll('textarea')[1].value;
+
+        let decodedText = ''
+
+        for (let index = 0; index < currentText.length; index++) {
+            const currentChar = currentText[index];
+            const charCode = currentChar.charCodeAt(0);
+            const newChar = String.fromCharCode(charCode - 1);
+
+            decodedText += newChar; 
+        }
+
+        document.querySelectorAll('textarea')[1].value = decodedText;
+    }
 }
