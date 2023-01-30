cf365b8bde43f86d224188bb99191d86f686730b
gyurel <gyurel@yahoo.com>
2023-01-30 13:50:54 +0100

    problem 3 Manipulations and Events

--- Exercise DOM Manipulation and Events/03. Locked-Profile/app.js
+++ Exercise DOM Manipulation and Events/03. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let divProfiles = Array.from(document.querySelectorAll('.profile'));
+    
+    for (const div of divProfiles) {
+        div.querySelector('button').addEventListener('click', onClick);
+        // debugger
+    }
+
+    function onClick(event){
+        let currentDivLockStatusUnlocked = event.target.parentElement.querySelector('input[value="unlock"]');
+        // debugger
+        let currentButton = event.target.parentElement.querySelector('button');
+        let currentHiddenDiv = event.target.parentElement.querySelector('div');
+        // debugger
+
+        if(currentDivLockStatusUnlocked.checked && currentButton.textContent == 'Show more'){
+            currentHiddenDiv.style.display = 'block';
+            currentButton.textContent = 'Hide it';
+        }else if(currentDivLockStatusUnlocked.checked && currentButton.textContent == 'Hide it'){
+            currentHiddenDiv.style.display = '';
+            currentButton.textContent = 'Show more';
+        }
+    }
 }
