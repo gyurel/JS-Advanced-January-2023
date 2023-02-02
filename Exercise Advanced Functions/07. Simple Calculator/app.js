7e4c0a86ffaf6ac6d68e02913c0615ce4a126ad1
gyurel <gyurel@yahoo.com>
2023-02-02 14:58:30 +0100

    problem 8 Advanced Functions

--- Exercise Advanced Functions/07. Simple Calculator/app.js
+++ Exercise Advanced Functions/07. Simple Calculator/app.js
@@ -1,7 +1,30 @@
 function calculator() {
-    // TODO:
+    let num1;
+    let num2;
+    let resultField;
+
+
+    return {
+        init: function (selector1, selector2, resultSelector){
+            num1 = document.querySelector(selector1);
+            num2 = document.querySelector(selector2);
+            resultField = document.querySelector(resultSelector);
+            return num1, num2, resultField
+        },
+    
+        
+        add: function (){
+            debugger
+            let result = Number(num1.value ) + Number(num2.value);
+            resultField.value = result;
+        },
+    
+        subtract: function (){
+            let result = Number(num1.value ) - Number(num2.value);
+            resultField.value = result;
+            },
+    } 
 }
 
-
-
-
+const calculate = calculator (); 
+calculate.init ('#num1', '#num2', '#result'); 
--- Exercise Advanced Functions/08. Next Article/script.js
+++ Exercise Advanced Functions/08. Next Article/script.js
@@ -1,3 +1,14 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let articlesArr = articles;
+    let articlesDiv = document.getElementById('content');
+
+    return () => {
+        let currentArticle = articlesArr.shift();
+        debugger
+        if(currentArticle){
+            let newArticleElement = document.createElement('article');
+        newArticleElement.textContent = currentArticle;
+        articlesDiv.appendChild(newArticleElement);
+        }
+    }
 }
