05ae54a94377c1b57223693f287e59d7fb8178a5
gyurel <gyurel@yahoo.com>
2023-02-06 00:51:10 +0100

    problem 6 Unit Testing

--- Exercise Unit Testing/06.Dynamic-Validation/app.js
+++ Exercise Unit Testing/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
+    const emailField = document.getElementById('email');
+    emailField.addEventListener('change', onchange);
+    const validMailRegEx = /([a-z]+)@([a-z]+)\.([a-z]+)/;
+
+
+    function onchange(){
+        debugger
+        if(!validMailRegEx.test(emailField.value) && emailField.value != ""){
+            emailField.classList.add('error');
+        }else{
+            emailField.classList.remove('error');
+        }
+    }
 }
